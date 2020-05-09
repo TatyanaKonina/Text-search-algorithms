09ac4b250b6b5fd6b0476b2a8e543b2b59a1f116
TatianaKonina <tdkonina@edu.hse.ru>
2020-05-09 17:41:23 +0300

    new structs for parser

--- structs.h
+++ structs.h
@@ -17,7 +17,7 @@ typedef struct {
 } SearchRequest;
 
 typedef struct {
-	int *matchedShifts;
+	int* matchedShifts;
 	int numberOfMatches;
 	int numOfCompares;
 	int numOfExtraOps;
@@ -25,4 +25,19 @@ typedef struct {
 	int memoryWaste;
 } SearchResult;
 
-#endif
+typedef enum Errors {
+	ERROR_OPEN_PYFILE = -1,
+	ERROR_MODULE_OBJECT = -2,
+	ERROR_DICT_OBJECT = -3,
+	ERROR_VALUE = -4,
+	ERROR_FUNC = -5,
+	ERROR_CALL_FUNC = -6,
+	ERROR_OPEN_FILE = -7,
+	ERROR_TEXT_TYPE = -8
+}Errors;
+
+typedef enum Text_type {
+	STROKE = 0,
+	TEXT = 1,
+	BOOK = 2,
+}Text_type;
