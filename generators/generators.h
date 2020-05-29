c08df8b0dc7464c1c7e387d74fc955f0d20ce911
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-05-29 20:23:28 +0300

    Update generators.h

--- generators/generators.h
+++ generators/generators.h
@@ -3,9 +3,9 @@
 
 void quick_sort(int* random_positions, int first, int last);
 
-char* text_compiling(char** data, int line_num, int pattern_num, char* pattern,int shift);
+char* text_compiling(char** data, int line_num, int pattern_num, char* pattern,int shift, int words_num);
 
-char* string_compiling(char** alf, float* probability, int line_num,int shift);
+char* string_compiling(char** alf, float* probability, int line_num,int shift,int sim_num);
 
 
 #endif 
