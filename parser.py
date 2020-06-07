3d2a0d629f8a576dde8e6b1c1f43e068855868a3
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-06-07 09:43:44 +0300

    fix bags with tabs,size and links

--- parser.py
+++ parser.py
@@ -5,37 +5,40 @@ import random
 def make_html(url):
     return BeautifulSoup(requests.get(url).content, 'lxml')
 
-def book_parser ():
+def book_parser (size):
     s =""
-    url = 'http://www.loyalbooks.com/Top_100'
+    url = 'http://www.loyalbooks.com/Top_100/'
     main_link = 'http://www.loyalbooks.com'
 
-    random_link = random.randint(1, 50)
-    if (random_link != 1):
-        url = url + '/' + str(random_link) + '/'
 
     make_html(url)
     book = make_html(url).find('table', class_='layout2-blue').find_all('td',
                                                                     class_='layout2-blue')  # получили все книги на странице
-    i = random.randint(0, 99)
+    i = random.randint(0,50)
 
     while (s == ""):
         if  (len(book) - 1 > i) & (book[i].find('a') != -1):
-            book_link = book[i].find('a').get('href')  # первая ссылка
-            name = book[i].find('b').text  # нашли название книги
-            link = main_link + book_link
-            page_book_html = make_html(link)  # перешли на страницу книги
-            if len(page_book_html.find_all('table',
+            print(book[i].find('a'))
+            book_link = book[i].find('a')# первая ссылка
+            if(book_link != None):
+                book_link = book[i].find('a').get('href')
+                name = book[i].find('b').text  # нашли название книги
+                link = main_link + book_link
+                page_book_html = make_html(link)  # перешли на страницу книги
+                if len(page_book_html.find_all('table',
                                        class_='book')) > 4:  # проверка на случай если на сайте нет онлайн версии
-                author = page_book_html.find('font', class_='book-author').text  # нашли автора
-                e_book_link = page_book_html.find_all('table', class_='book')[4].find_all('td', class_='book2')[2].find(
-                'a').get('href')
+                    author = page_book_html.find('font', class_='book-author').text  # нашли автора
+                    e_book_link = page_book_html.find_all('table', class_='book')[4].find_all('td', class_='book2')[2].find(
+                    'a').get('href')
             # нашли ссылку на которой онлайн книга
-                page_book_txt = make_html(e_book_link)
-                s = name + ' ' + author
-                text = page_book_txt.find_all('p')
-                for p in text:
-                    s = s + p.text
-                g = s.encode('utf-8')
-                return g
+                    page_book_txt = make_html(e_book_link)
+                    s = name + ' ' + author
+                    text = page_book_txt.find_all('p')
+                    for p in text:
+                        if (len(s) > size) :
+                            break
+                        s = s + p.text
+                        s = " ".join(re.split("\s+", s, flags=re.UNICODE))
+                    print(s)
+                    return s
         i = random.randint(0, 99)
