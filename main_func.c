96f7cb1529a1cb943f4451de6b31afbe206a527c
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-06-09 15:30:02 +0300

    add random pattern func

--- main_func.c
+++ main_func.c
@@ -10,10 +10,11 @@
 
 const int max_world_len = 10;
 const int str_size = 10;
+const int one_word = 1;
 
 #define ALPHSIZE 123
 #define BASE 256
-#define QMOD 101
+#define QMOD 13
 #define NAIMEM 1 //уникальный только matched
 #define RKMEM 4 //уникальные: matched, radix, patNumber, textNumber
 #define BMHMEM 127 //уникальные: ind, textInd, positionn, equal, badCharaters с размером ALPHSIZE + внутри функции считается динамически выделенная память под relatedshifts
@@ -24,27 +25,27 @@ extern const int alphabetAccess[];
 //-----------------------------------------------algorithms----------------------------------------------------------------------------------------------------------------
 SearchResult* boyerMooreHorspoolMatcher(SearchRequest* request)
 {
-	SearchResult *result;
+	SearchResult* result;
 	int shift = 1, i = 0, ind = 1, textInd = 0, position = (request->pattern->needleSize) - 1, equal = 0;
-	int badCharacters[ALPHSIZE]={0}, *relatedShifts;
-	char safeCharacter=0;
+	int badCharacters[ALPHSIZE] = { 0 }, * relatedShifts;
+	char safeCharacter = 0;
 	clock_t start, finish;
 	start = clock();
 
-	result = (SearchResult*) malloc(sizeof(SearchResult));
-	result->matchedShifts = (int*) calloc(request->text->haystackSize, sizeof(int));
+	result = (SearchResult*)malloc(sizeof(SearchResult));
+	result->matchedShifts = (int*)calloc(request->text->haystackSize, sizeof(int));
 	result->numberOfMatches = 0;
 	result->memoryWaste = BMHMEM * sizeof(int) + sizeof(int*) + request->pattern->needleSize * sizeof(int);
 	result->numOfCompares = 0;
 	result->numOfExtraOps = 0;
 
-	relatedShifts = (int*) calloc(request->pattern->needleSize, sizeof(int));
+	relatedShifts = (int*)calloc(request->pattern->needleSize, sizeof(int));
 
 	for (i = request->pattern->needleSize - 2; i >= 0; i--) {
 		if (badCharacters[request->pattern->needle[i]] < 1) {  //extra comparison
 			badCharacters[request->pattern->needle[i]] = ind;
-			relatedShifts[ind-1] = shift;
-			result->numOfExtraOps++; 
+			relatedShifts[ind - 1] = shift;
+			result->numOfExtraOps++;
 			ind++;
 		}
 		shift++;
@@ -52,53 +53,65 @@ SearchResult* boyerMooreHorspoolMatcher(SearchRequest* request)
 
 	shift = 0;
 
-	while (shift <= (request->text->haystackSize) - (request->pattern->needleSize) ) {
+	while (shift <= (request->text->haystackSize) - (request->pattern->needleSize)) {
 		ind = (request->pattern->needleSize) - 1;
 		textInd = position;
 		equal = 1;
 		safeCharacter = request->text->haystack[textInd];
 		while (equal && ind >= 0) {
-			if ( !(request->pattern->needle[ind] == request->text->haystack[textInd]) ) {
+			if (!(request->pattern->needle[ind] == request->text->haystack[textInd])) {
 				equal = 0;
 			}
 			else {
-			ind--;
-			textInd--;
+				ind--;
+				textInd--;
 			}
 			result->numOfCompares++;
 		}
 		if (equal) { //
 			result->matchedShifts[result->numberOfMatches++] = shift;
-		}
-		if (badCharacters[safeCharacter]) { //
-			shift += relatedShifts[badCharacters[safeCharacter] - 1];
-			position += relatedShifts[badCharacters[safeCharacter] - 1];
-			result->numOfExtraOps++; // extra comparison
+			if (badCharacters[safeCharacter]) { //
+				shift += relatedShifts[badCharacters[safeCharacter] - 1];
+				position += relatedShifts[badCharacters[safeCharacter] - 1];
+				result->numOfExtraOps += 2; //х2 extra comparison
+			}
+			else {
+				shift += request->pattern->needleSize;
+				position += request->pattern->needleSize;
+				result->numOfExtraOps += 2; //х2 extra comparison (failed if statement)
+			}
 		}
 		else {
-			shift += request->pattern->needleSize;
-			position += request->pattern->needleSize;
-			result->numOfExtraOps++; //extra comparison (failed if statement)
+			if (badCharacters[request->text->haystack[textInd]]) { //
+				shift += relatedShifts[badCharacters[request->text->haystack[textInd]] - 1];
+				position += relatedShifts[badCharacters[request->text->haystack[textInd]] - 1];
+				result->numOfExtraOps += 2; //х2 extra comparison (failed if(equal) statement)
+			}
+			else {
+				shift += request->pattern->needleSize;
+				position += request->pattern->needleSize;
+				result->numOfExtraOps += 2; //х2 extra comparison (failed if statement + failed if(equal) statement)
+			}
 		}
 	}
 
 	free(relatedShifts);
 	finish = clock();
-	result->workTime = (double)(finish -start) / CLOCKS_PER_SEC;
+	result->workTime = (double)(finish - start) / CLOCKS_PER_SEC;
 
 	return result;
 }
 
 SearchResult* naiveStringMatcher(SearchRequest* request)
 {
-	SearchResult *result;
+	SearchResult* result;
 	int shift = 0, matched = 0, i = 0;
 	clock_t start, finish;
 	start = clock();
 
 
-	result = (SearchResult*) malloc(sizeof(SearchResult));
-	result->matchedShifts = (int*) calloc(request->text->haystackSize, sizeof(int));
+	result = (SearchResult*)malloc(sizeof(SearchResult));
+	result->matchedShifts = (int*)calloc(request->text->haystackSize, sizeof(int));
 	result->memoryWaste = NAIMEM * sizeof(int);
 	result->numberOfMatches = 0;
 	result->numOfCompares = 0;
@@ -106,68 +119,60 @@ SearchResult* naiveStringMatcher(SearchRequest* request)
 
 	for (shift = 0; shift <= (request->text->haystackSize) - (request->pattern->needleSize); shift++) {
 		matched = 0;
-		for (i = 0; i < (request->pattern->needleSize); i++) { 																		
-			result->numOfCompares++;                       
-			if (request->pattern->needle[i] != request->text->haystack[shift + i]) {
-				i = request->pattern->needleSize;                                                           
-			}
-			else {
-				matched++; 
-			}
-		}																		
-		if (matched == request->pattern->needleSize ) {
+		for (i = 0; i < (request->pattern->needleSize); i++) {                        //
+			if (request->pattern->needle[i] == request->text->haystack[shift + i]) {  //
+				matched++;                                                            //может переделать цикл и дбавить в него if c break, чтобы лишних итераций не капало?
+			}																		  //
+			result->numOfCompares++;											      //
+		}																			  //
+		if (matched == request->pattern->needleSize) {
 			result->matchedShifts[result->numberOfMatches++] = shift;
 		}
 	}
 
 	finish = clock();
-	result->workTime = (double)(finish -start) / CLOCKS_PER_SEC;
+	result->workTime = (double)(finish - start) / CLOCKS_PER_SEC;
 
 	return result;
 }
 
 SearchResult* rabinKarpMatcher(SearchRequest* request)
 {
-	SearchResult *result;
-	int patNumber = 0, textNumber = 0, shift = 0, matched = 0, i = 0, radix = 1; 
+	SearchResult* result;
+	int radix = 0, patNumber = 0, textNumber = 0, shift = 0, matched = 0, i = 0;
 	clock_t start, finish;
 	start = clock();
 
-	result = (SearchResult*) malloc(sizeof(SearchResult));
-	result->matchedShifts = (int*) calloc(request->text->haystackSize, sizeof(int));
+	result = (SearchResult*)malloc(sizeof(SearchResult));
+	result->matchedShifts = (int*)calloc(request->text->haystackSize, sizeof(int));
 	result->memoryWaste = RKMEM * sizeof(int);
 	result->numberOfMatches = 0;
 	result->numOfCompares = 0;
 	result->numOfExtraOps = 0;
 
-	for (i = 0; i < request->pattern->needleSize - 1; i++) {
-		radix = (radix * BASE) % QMOD;
-	}
+	radix = (int)pow((float)BASE, request->pattern->needleSize - 1) % QMOD;
 
 	for (i = 0; i < request->pattern->needleSize; i++) {
 		patNumber = (BASE * patNumber + request->pattern->needle[i]) % QMOD;
 		textNumber = (BASE * textNumber + request->text->haystack[i]) % QMOD;
-		result->numOfExtraOps += 2;
+		result->numOfExtraOps += 2; //ну тут просто хэши создаются циклом, может тоже считать за extra operation?
 	}
 
-	for (shift = 0; shift <= (request->text->haystackSize) - (request->pattern->needleSize) ; shift++) {
+	for (shift = 0; shift <= (request->text->haystackSize) - (request->pattern->needleSize); shift++) {
 		matched = 0;
 		if (patNumber == textNumber) {
-			for (i = 0; i < request->pattern->needleSize; i++) {                         
-				result->numOfCompares++;                       
-				if (request->pattern->needle[i] != request->text->haystack[shift + i]) {
-					i = request->pattern->needleSize;                                                           
-				}
-				else {
-					matched++; 
-				}                                                                    
-			}                                                                            
+			for (i = 0; i < request->pattern->needleSize; i++) {                         //
+				if (request->pattern->needle[i] == request->text->haystack[shift + i]) { //
+					matched++;                                                           //может переделать цикл и дбавить в него if c break, чтобы лишних итераций не капало?
+					result->numOfCompares++;                                             //
+				}                                                                        //
+			}                                                                            //
 			if (matched == request->pattern->needleSize) {
 				result->matchedShifts[result->numberOfMatches++] = shift;
 			}
 			result->numOfExtraOps++; //comparisons of hashes
 		}
-		if (shift <= (request->text->haystackSize) - (request->pattern->needleSize) ) {
+		if (shift <= (request->text->haystackSize) - (request->pattern->needleSize)) { //это сравнение вроде как лишнее, так как задано в цикле, но в книге оно было. Оставлять или не нужно?
 			textNumber = (BASE * (textNumber - (request->text->haystack[shift]) * radix) + request->text->haystack[shift + request->pattern->needleSize]) % QMOD;
 			result->numOfExtraOps++;
 		}
@@ -178,7 +183,7 @@ SearchResult* rabinKarpMatcher(SearchRequest* request)
 	}
 
 	finish = clock();
-	result->workTime = (double)(finish -start) / CLOCKS_PER_SEC;
+	result->workTime = (double)(finish - start) / CLOCKS_PER_SEC;
 
 	return result;
 }
@@ -313,7 +318,7 @@ char* string_compiling(char** alf, float* probability, int line_num, int shift, 
 	}
 	int random_size = (int)tempt[line_num - 2];
 	srand(time(NULL) + shift);
-	for (int i = 0; i < sim_num - 1; i++) {
+	for (int i = 0; i < sim_num ; i++) {
 		rand_digit = rand() % (random_size + 1);
 		for (int j = 0; j < line_num - 1; j++) {//проверка на подходящий диапазон
 			if ((int)tempt[j] >= rand_digit) {
@@ -373,10 +378,12 @@ char* text_compiling(char** data, int line_num, int patterns_num, char* pattern,
 		quick_sort(random_positions, 0, patterns_num - 1);//сортируем
 	}
 	for (int i = 0; i < words_num; i++) {
-		if ((i == random_positions[pointer]) && patterns_num) {
-			pointer++;               // если попали на               
-			strcat(text, pattern);   //рандомную позиция                      
-			strcat(text, " ");       // вставляем паттерн
+		if ( patterns_num) {
+			if ((i == random_positions[pointer])) {
+				pointer++;               // если попали на               
+				strcat(text, pattern);   //рандомную позиция                      
+				strcat(text, " ");       // вставляем паттерн
+			}
 		}
 		else {
 			int r = rand() % line_num;
@@ -391,8 +398,8 @@ char* text_compiling(char** data, int line_num, int patterns_num, char* pattern,
 }
 
 //----------------------------------------------dll-func---------------------------------------------------------------------------------------------------------------------
+
 DLL_EXPORT SearchRequest* make_text_storage (int texts_num, int text_type, char* file_in, char* pattern, int pattern_num, int num) {
-	
     //возвращает массив тестов
     SearchRequest* storage = (SearchRequest*)malloc(texts_num * sizeof(SearchRequest));
 	char* text;
@@ -407,19 +414,24 @@ DLL_EXPORT SearchRequest* make_text_storage (int texts_num, int text_type, char*
         }
         float* probability = (float*)calloc(line_num, sizeof(float));
         processing_file_with_probability(data, alf, probability, line_num);
+        if (pattern == NULL) {
+			pattern = string_compiling(alf, probability, line_num, NULL, 1 + rand() % max_world_len);
+		}
 		for (int i = 0; i < texts_num; i++) {
 			text = string_compiling(alf, probability, line_num, i, num);
+			
 			Init_Memory_Request(&storage[i], text_type, pattern,text);
-			//storage[i].text->haystack = string_compiling(alf, probability, line_num, i, num);
-			//storage[i].text->haystackSize = strlen(storage[i].text->haystack);
 		}
         break;
     }
     case TEXT: {
         int line_num = words_num_in_file(file_in);
         char** data = read_data(file_in, line_num);
+        if (pattern == NULL) {
+			pattern = text_compiling(data, line_num, NULL, NULL, NULL, one_word);
+		}
         for (int i = 0; i < texts_num; i++) {
-	    text = text_compiling(data, line_num, pattern_num, pattern, i, num);
+			text = text_compiling(data, line_num, pattern_num, pattern, i, num);
             Init_Memory_Request(&storage[i], text_type, pattern,text);
 
         }
@@ -443,6 +455,7 @@ void Init_Memory_Request(SearchRequest* storage, int text_type, char* pattern,ch
 }
 
 DLL_EXPORT SearchRequest* make_result_storage(SearchRequest* texts_storage, int algorithm_type, int texts_num) {
+	
     // прогоняет алгоритмы на всех текстах (на вход масссив тестов)
     //возвращает массив searchReasult
     SearchResult* storage = (SearchResult*)malloc(texts_num * sizeof(SearchResult));
@@ -450,6 +463,7 @@ DLL_EXPORT SearchRequest* make_result_storage(SearchRequest* texts_storage, int 
 		switch (algorithm_type) {
 		case BMHM: {
 			storage[i] = *(boyerMooreHorspoolMatcher(&texts_storage[i]));
+			
 			break;
 		}
 		case NAIVE: {
@@ -478,8 +492,9 @@ DLL_EXPORT SearchResult* make_statictic(SearchResult* result_storage, int texts_
     StaticticResult->memoryWaste = 0;
     StaticticResult->numOfCompares = 0;
     StaticticResult->numOfExtraOps = 0;
+	
     for (int i = 0; i < texts_num; i++) {
-
+        
         StaticticResult->numberOfMatches += result_storage[i].numberOfMatches;
         StaticticResult->numOfCompares += result_storage[i].numOfCompares;
         StaticticResult->numOfExtraOps += result_storage[i].numOfExtraOps;
@@ -494,6 +509,7 @@ DLL_EXPORT SearchResult* make_statictic(SearchResult* result_storage, int texts_
     StaticticResult->numberOfMatches = StaticticResult->numberOfMatches / texts_num;
     
     StaticticResult->memoryWaste = StaticticResult->memoryWaste / texts_num;
+	
 
     return StaticticResult;
 }
@@ -501,6 +517,14 @@ DLL_EXPORT SearchResult* make_statictic(SearchResult* result_storage, int texts_
 DLL_EXPORT SearchRequest* make_parser_storage(char ** text, char* pattern, int texts_num,int len) {
 	len *= 10;
 	SearchRequest* storage = (SearchRequest*)malloc(texts_num * sizeof(SearchRequest));
+    if (pattern == NULL) {
+		char * istr = strtok(text[0], " ");
+		for (int i = 0; i < rand() % len; i++) {
+			istr = strtok(NULL, " ");
+			
+		}
+		pattern = istr;
+	}
 	for (int i = 0; i < texts_num; i++) {
 		Init_Memory_Request(&storage[i], BOOK, pattern,text[i]);
 		if (storage[i].text->haystackSize > len) {
